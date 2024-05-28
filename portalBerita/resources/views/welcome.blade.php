974afcf6a2f67a5a0b2bda23dff92fb229c9b076
unknown <khodrijasnur@gmail.com>
2024-05-28 23:49:48 +0700

    tugas

---
+++ portalBerita/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+    Table
+@endsection
+
+@section('content')
+<ul>
+    <li>Link 1</li>
+    <li>Link 2<a href="/daftar">Klik disini</a></li>
+    <li>Link 3</li>
+</ul>
+@endsection
